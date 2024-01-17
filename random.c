7faaa0f52bc5892d8aec263477a888f834956d43
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2024-01-17 10:12:28 -0500

    Create main to test randchar function
    use srand with current time as the input for the random number generator. ASCII number 90 is Z and 65 is A so generate the random integer between these values.

--- random.c
+++ random.c
@@ -1,8 +1,20 @@
 //
 // Created by Mihir Patel on 1/17/24.
 //
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 char randchar(){
-    
+    srand(time(0));
+    char c;
+    int num = (rand() % (90 - 65 + 1)) + 65;
+    c = num;
+    return c;
 }
+
+int main(){
+    char c  = randchar();
+    printf("%c", c);
+}
+
